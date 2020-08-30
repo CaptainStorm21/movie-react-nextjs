067ef350e2cb1cbe9e9e840a3c02a009583e7217
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-08-30 16:30:23 -0700

    class conversion

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,27 @@
-//functional component
-const About = () => {
-    const message = "From React world"
-    return (
-        <h1> Hello {message} </h1>
-    )
+import React, { Component } from 'react';
+
+class About extends React.Component {
+    render() {
+        return (
+            <h1> Welcome </h1>
+            )
+    }
 }
 
 export default About
+
+//functional component
+/*
+ * when to use
+ *   1. for smaller components
+ *   2. reusable components
+ *   3. presentational commponents, partially right we can use state c
+ */
+//const About = () => {
+//    const message = "From React world"
+//    return (
+//        <h1> Hello {message} </h1>
+//    )
+//}
+
+//export default About

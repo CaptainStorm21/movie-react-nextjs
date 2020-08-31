61a8b314c796f22ff23cc24d704ecfecaee17dbe
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-08-30 22:27:46 -0700

    set up constructor and buttons

--- components/movie-list.js
+++ components/movie-list.js
@@ -1,23 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const MovieList = () => {
-    return (
-        <div className="col-lg-4 col-md-6 mb-4">
-            <div className="card h-100">
-                <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
-                <div className="card-body">
-                    <h4 className="card-title">
-                        <a href="#">Item One</a>
-                    </h4>
-                    <h5>$24.99</h5>
-                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+class MovieList extends Component {
+
+    //state = {
+    //    count: 0,
+
+    //}
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 0
+        }
+    }
+
+    increment = () => {
+        alert("Add ")
+    }
+
+    decrement = () => {
+        alert("Remove ")
+    }
+
+    render() {
+        return (
+            <>
+                <div className="col-lg-4 col-md-6 mb-4">
+                    <div className="card h-100">
+                        <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
+                        <div className="card-body">
+                            <h4 className="card-title">
+                                <a href="#">Item One</a>
+                            </h4>
+                            <h5>$24.99</h5>
+                            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                        </div>
+                        <div className="p-2">
+                            <button className="btn btn-primary mx-2" onClick={this.increment}> Add </button>
+                            <button className="btn btn-warning" onClick={this.decrement}> Remove </button>
+                        </div>
+                        <h2>{this.state.count}</h2>
+                    </div>
                 </div>
-                <div className="card-footer">
-                    <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                </div>
-            </div>
-        </div>
-    );
+            </>
+        );
+    }
 }
-
-export default MovieList;
+    export default MovieList;

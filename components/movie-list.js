688a80cc5bdacd054f9b5c4fb0fe506529448feb
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-09-05 23:51:36 -0700

    iterating through list of data

--- components/movie-list.js
+++ components/movie-list.js
@@ -1,37 +1,43 @@
 import React, { Component } from 'react';
 
 class MovieList extends Component {
-    render() {
-        const { movies } = this.props
 
-        return (
-            <>
-                {movies.map((movie) => {
-                    return (<div className="col-lg-4 col-md-6 mb-4">
-                        <div className="card h-100">
-                            <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
-                            <div className="card-body">
-                                <h4 className="card-title">
-                                    <a href="#">{movie.name}</a>
-                                </h4>
-                                <h5>${movie.price }</h5>
-                                <p className="card-text">{movie.description}</p>
-                                <small> Release Year - {movie.releaseYear}</small>
-                            </div>
-                            <div className="card-footer">
-                                <small className="text-muted">{movie.rating}</small>
-                                <br />
-                                <small className="text-muted">{movie.genre}</small>
+    renderMovies(movies) {
 
-                            </div>
-                            <h2>{this.props.count}</h2>
+        return movies.map(movie =>
+            (
+                <div className="col-lg-4 col-md-6 mb-4">
+                    <div className="card h-100">
+                        <a href="#"><img className="card-img-top"
+                            src={movie.image} alt="" /></a>
+                        <div className="card-body">
+                            <h4 className="card-title">
+                                <a href="#">{movie.name}</a>
+                            </h4>
+                            <h5>${movie.price}</h5>
+                            <p className="card-text">{movie.description}</p>
+                            <small> Release Year - {movie.releaseYear}</small>
                         </div>
+                        <div className="card-footer">
+                            <small className="text-muted">{movie.rating}</small>
+                            <br />
+                            <small className="text-muted">Genre: {movie.genre}</small>
+                        </div>
+                        <h2>{this.props.count}</h2>
                     </div>
-                    )
-                })
-                }
-            </>
-        );
+                </div>
+            )
+        )
+    }
+
+    render() {
+        const { movies } = this.props;
+        return (
+            <React.Fragment>
+                { this.renderMovies(movies)}
+            </React.Fragment>
+        )
     }
 }
+
 export default MovieList;

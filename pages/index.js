92247d3018461c2108fedf24997e0072caa0d26a
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-09-07 23:52:39 -0700

    converting into class home component/index.js

--- pages/index.js
+++ pages/index.js
@@ -9,73 +9,147 @@ import Sidebar from '../components/sidebar';
 //importing MOVIE DATA function
 import { getMovies } from '../actions';
 
-const Home = () => {
+class Home extends React.Component {
+    //const[movies, setMovies] = useState([]);
 
-    const [movies, setMovies] = useState([]);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const resMovies = await getMovies()
-            setMovies(resMovies)
+    constructor(props) {
+        super(props)
+        this.state = {
+            movies: []
         }
-    fetchData();
-    })
+    }
 
+    componentDidMount() {
+        getMovies().then((movies) => {
+            this.setState({ movies })
+        })
+    }
+    /*    state = {
+            movies: []
+        }*/
 
-    //debugger
-    //imporve this because now it is called
-    //empty array if nothing is found
-    //const movies = getMovies() || []
-    //debugger
+    //called only once when component is mounted
+    //async componentDidMount() {
+    //    const movies = await getMovies()
+    //    this.setState({ movies })
+    //}
 
-    ////useEffect
     //useEffect(() => {
-    //    getMovies().then((movies) => {
-    //        //debugger
-    //        setMovies(movies)
-    //    })
-    //}, [])
+    //    const fetchData = async () => {
+    //        const resMovies = await getMovies()
+    //        setMovies(resMovies)
+    //    }
+    //    fetchData();
+    //});
 
-    //debugger
+    render() {
+        const { movies } = this.state
+        return (
+            <div>
+                <Head>
+                    <title>Home</title>
+                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous" />
+                    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
+                    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossOrigin="anonymous"></script>
+                    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossOrigin="anonymous"></script>
+                </Head>
+                <Navbar />
 
-    return (
-        <div>
-            <Head>
-                <title>Home</title>
-                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous" />
-                <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossOrigin="anonymous"></script>
-                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossOrigin="anonymous"></script>
-            </Head>
-            <Navbar />
+                <div className="container-fluid home-page">
 
-            <div className="container-fluid home-page">
-
-                <div className="row mt-4">
-                    <Sidebar
-                        appName="Movie Archive"
-                    />
-                    <div className="col-lg-9">
-                        <Carausel />
-                        <div className="row">
-                            <MovieList
-                                movies={movies}
-                            />
+                    <div className="row mt-4">
+                        <Sidebar
+                            appName="Movie Archive"
+                        />
+                        <div className="col-lg-9">
+                            <Carausel />
+                            <div className="row">
+                                <MovieList
+                                    movies={movies}
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <Footer />
+                <Footer />
 
-            <style jsx>
-                {`
+                <style jsx>
+                    {`
           .home-page {
                background-color: #fafafa;
                 margin-top: 30px;
         `}
-            </style>
-        </div >
-    )
+                </style>
+            </div >
+        )
+    }
 }
+
+//const Home = () => {
+//    const [movies, setMovies] = useState([]);
+
+//    useEffect(() => {
+//        const fetchData = async () => {
+//            const resMovies = await getMovies()
+//            setMovies(resMovies)
+//        }
+//        fetchData();
+//    });
+
+//    //debugger
+//    //imporve this because now it is called
+//    //empty array if nothing is found
+//    //const movies = getMovies() || []
+//    //debugger
+
+//    ////useEffect
+//    //useEffect(() => {
+//    //    getMovies().then((movies) => {
+//    //        //debugger
+//    //        setMovies(movies)
+//    //    })
+//    //}, [])
+
+//    //debugger
+
+//    return (
+//        <div>
+//            <Head>
+//                <title>Home</title>
+//                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous" />
+//                <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
+//                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossOrigin="anonymous"></script>
+//                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossOrigin="anonymous"></script>
+//            </Head>
+//            <Navbar />
+
+//            <div className="container-fluid home-page">
+
+//                <div className="row mt-4">
+//                    <Sidebar
+//                        appName="Movie Archive"
+//                    />
+//                    <div className="col-lg-9">
+//                        <Carausel />
+//                        <div className="row">
+//                            <MovieList
+//                                movies={movies}
+//                            />
+//                        </div>
+//                    </div>
+//                </div>
+//            </div>
+//            <Footer />
+
+//            <style jsx>
+//                {`
+//          .home-page {
+//               background-color: #fafafa;
+//                margin-top: 30px;
+//        `}
+//            </style>
+//        </div >
+//    )
+//}
 
 export default Home;
